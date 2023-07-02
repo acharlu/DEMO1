f802c89fc833108f85efa8f7609c61afbfde9c0c
akshay <acharlu7@gamil.com>
2023-07-02 13:23:07 +0530

    # Read_CSV

--- Read_CSvV.py
+++ Read_CSvV.py
@@ -2,3 +2,15 @@ import pandas as pd
 
 read = pd.read_csv("organizations-100.csv")
 print(read)
+
+#Size (How many items are there)
+print(read.size)
+#Shape (How many items are there in the form of rows and coloumn)
+print(read.shape)
+#Count (how many items by each coloum ,give us full details
+print(read.count())
+#DataTyepe(Give datatype of every object)
+print(read.dtypes)
+#unique (cheack a number unque items)
+print(read.nunique())
+
